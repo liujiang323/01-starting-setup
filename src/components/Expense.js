7aa7e39a409b34fe372f33499de6490e430e6c3b
jiang_liu5 <jiang_liu4@apple.com>
2023-07-13 22:23:28 -0500

    using children props to construct composition

---
+++ src/components/Expense.js
@@ -0,0 +1,16 @@
+import ExpenseItem from "./ExpenseItem";
+import './Expense.css';
+import Card from "./Card";
+
+function Expense(props) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem expense={props.item[0]} />
+      <ExpenseItem expense={props.item[1]} />
+      <ExpenseItem expense={props.item[2]} />
+      <ExpenseItem expense={props.item[3]} />
+    </Card>
+  );
+}
+
+export default Expense;
