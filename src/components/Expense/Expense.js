1043b128770a307c14e19560a0653cfa9d2b4df4
jiang_liu5 <jiang_liu4@apple.com>
2023-07-16 15:22:48 -0500

    added ExpensesFilter component

--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -1,10 +1,17 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expense.css';
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 function Expense(props) {
+  const[filteredYear, setFilteredYear] = useState('2020');
+  const onFilterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter selectValue={filteredYear} onFilterChange={onFilterChangeHandler} />
       <ExpenseItem expense={props.expenseItems[0]} />
       <ExpenseItem expense={props.expenseItems[1]} />
       <ExpenseItem expense={props.expenseItems[2]} />
