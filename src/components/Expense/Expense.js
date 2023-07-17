1935306011e894479ecaca0a0fb2e371d995478c
jiang_liu5 <jiang_liu4@apple.com>
2023-07-17 16:18:35 -0500

    Added dynamic chart to show up data

--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expense(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -19,6 +20,7 @@ function Expense(props) {
         selectValue={filteredYear}
         onFilterChange={onFilterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
