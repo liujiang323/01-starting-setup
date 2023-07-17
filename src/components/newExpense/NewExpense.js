4f523cb63cd1cb17c9cf0cb2f8a95107cdd164a4
jiang_liu5 <jiang_liu4@apple.com>
2023-07-17 13:02:06 -0500

    Added condition to show up new expenses form

--- src/components/newExpense/NewExpense.js
+++ src/components/newExpense/NewExpense.js
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isShowingForm, setIsShowingForm] = useState(false);
+
   const saveExpenseHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
     props.onAddExpense(expenseData);
+    setIsShowingForm(false);
   };
+
+  const formShowupHandler = () => {
+    setIsShowingForm(true);
+  };
+
+  const cancelFormHandler = (isFormShowing) => {
+    setIsShowingForm(false);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseHandler} />
+      {isShowingForm && <ExpenseForm onSaveExpenseData={saveExpenseHandler} onCancel={cancelFormHandler} />}
+      {!isShowingForm && <button onClick={formShowupHandler}>Add New Expenses</button>}
     </div>
   );
 };
